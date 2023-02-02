579120eae596f888a09f9f4be2d91ad086cc9c84
Zelv <contact@elv-studio.com>
2023-02-02 20:12:05 +0100

    use absolute path to compile scss files

--- config.py
+++ config.py
@@ -11,9 +11,6 @@ CONTACT = 'gael.le-godais@univ-grenoble-alpes.fr'
 DATABASE = 'instance/test_P5_D4_RT_003_cam.db'
 db = peewee.SqliteDatabase(DATABASE)
 
-# compile scss files into css folder
-sass.compile(dirname=('static/scss', 'static/css'), output_style='compressed')
-
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.secret_key = "3urg0459"
@@ -30,3 +27,8 @@ def after_request(response):
 
 EXPERIMENT = os.path.join(app.root_path, EXPERIMENT)
 experiment = Experiment(EXPERIMENT)
+
+# compile scss files into css folder
+scss_dir = os.path.join(app.root_path, 'static/scss')
+css_dir = os.path.join(app.root_path, 'static/css')
+sass.compile(dirname=(scss_dir, css_dir), output_style='compressed')
